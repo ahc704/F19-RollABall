64f92fdbed3f3efa020db61d76e7f513cc9a1db4
ahc704 <46978022+ahc704@users.noreply.github.com>
2019-09-15 15:38:39 -0400

    Step 8: Added Counter and Win Text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-
     public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();    
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -28,6 +34,17 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag ("Pickup"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
